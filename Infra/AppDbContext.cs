34e71cf141292821c25b50ca73a83bfbd2bca805
joao <joaovictorsantanaf@gmail.com>
2025-05-15 16:33:53 -0300

    update release-v0.1

--- Infra/AppDbContext.cs
+++ Infra/AppDbContext.cs
@@ -8,8 +8,9 @@ namespace TCC_WPF.Infra
         public DbSet<UserData> UserDatas { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            // Exemplo de string de conexão PostgreSQL
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=TCC_Test;Username=postgres;Password=postgres");
+            // ALTERE ESTA STRING SE ESTIVER USANDO OUTRA INSTÂNCIA DO POSTGRES
+            var connectionString = "Host=localhost;Port=5432;Database=TCC_Test;Username=postgres;Password=postgres";
+            optionsBuilder.UseNpgsql(connectionString);
         }
     }
 }
